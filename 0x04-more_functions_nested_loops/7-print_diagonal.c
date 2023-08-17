a8f4416eaa7efbdb8110adb790fc9ba0468a7594
kachisapphire <mychaelesther99@gmail.com>
2023-08-17 09:38:22 -0700

    ./7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_diagonal - entry point
+ *@n: number of times the number is printed
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
