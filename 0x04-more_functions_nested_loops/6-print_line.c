518e30843379a304b55c1a011e61977e43c5fdc8
kachisapphire <mychaelesther99@gmail.com>
2023-08-17 08:59:47 -0700

    ./6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_line - entry point
+ *@n: the number of times to draw
+ *Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
