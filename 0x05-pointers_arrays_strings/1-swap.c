f89d3db9627e8585aa43aee0230e36b3f7536efc
kachisapphire <mychaelesther99@gmail.com>
2023-08-21 12:53:47 -0700

    ./1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - entry point
+ *@a: first integer
+ *@b: second integer
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
 
+	*a = *b;
+	*b = tmp;
+}
