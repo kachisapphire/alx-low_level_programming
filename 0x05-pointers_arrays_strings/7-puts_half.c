711dabb9c94f813779a1e8d9564a169568f41a95
kachisapphire <mychaelesther99@gmail.com>
2023-08-21 21:59:08 -0700

    ./7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *puts_half - entry point
+ *@str: string to print
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+		j++;
+	if (j % 2 != 0)
+	{
+		k = ((j - 1) / 2);
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+	for (; k < j; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
