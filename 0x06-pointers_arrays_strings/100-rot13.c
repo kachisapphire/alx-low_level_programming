dfd042ee61391a8daeb358931f585b44bd216e94
kachisapphire <mychaelesther99@gmail.com>
2023-08-24 08:29:10 -0700

    ./100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *rot13 - entry point
+ *@s: string to be encoded
+ *Return: s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
