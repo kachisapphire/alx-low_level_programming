323fd3cf99d93418cb4d74d2b7466214990d3a0c
kachisapphire <mychaelesther99@gmail.com>
2023-08-29 04:45:28 -0700

    ./5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strstr - entry point
+ *@needle: substring
+ *@haystack: string
+ *Return: haystack or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
