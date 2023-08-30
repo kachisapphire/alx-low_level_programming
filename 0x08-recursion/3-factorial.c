4538ddc831def1203b5eecfabfe62fbc8f6858dd
kachisapphire <mychaelesther99@gmail.com>
2023-08-30 04:55:12 -0700

    ./3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *factorial - entry point
+ *@n: given number
+ *Return: factorial or -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
