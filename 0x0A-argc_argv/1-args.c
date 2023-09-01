d613c9a074a810d3262af3e0caace538fcb235db
kachisapphire <mychaelesther99@gmail.com>
2023-09-01 09:25:25 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argv: argument vector
+ *@argc: argument count
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
