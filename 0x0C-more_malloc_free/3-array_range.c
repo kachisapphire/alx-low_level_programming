2919bd4e408da7bebd7a96db042b179975776a48
kachisapphire <mychaelesther99@gmail.com>
2023-09-07 09:29:28 -0700

    ./3-array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *array_range - entry point
+ *@min: first input
+ *@max: second input
+ *Return: a
+ */
+int *array_range(int min, int max)
+{
+	int *a, i = 0;
+
+	if (min > max)
+		return (NULL);
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
+	if (a == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+	return (a);
+}
