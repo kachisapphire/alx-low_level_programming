c06c0fd9f18fe469043abe21f81828651d669013
kachisapphire <mychaelesther99@gmail.com>
2023-09-14 00:14:08 -0700

    womans constant

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ *print_strings - entry point
+ *@separator: string between string
+ *@n: number of strings
+ *Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
