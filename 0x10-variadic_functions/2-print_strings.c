8172f39a504c0e47b0277f87821a05cb41b978dd
kachisapphire <mychaelesther99@gmail.com>
2023-09-14 04:29:26 -0700

    constant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str != NULL)
 			printf("%s", str);
 		else
-			printf("(nil");
+			printf("(nil)");
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
