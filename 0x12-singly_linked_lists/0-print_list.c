b10c601894b2b2e78d5be08f1f6f0efbee50db91
kachisapphire <jarapolice03@gmail.com>
2023-09-21 11:59:23 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nodes;
+
+	nodes = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d]%s\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d]%s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		nodes++;
---
+++ 0x12-singly_linked_lists/2-main.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ *add_node - function to execute
+ *@head: beginning of the list
+ *@str: string to print
+ *Return: element or NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
