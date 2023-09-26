e8520dc7fd4fb91ba94abf580e9534c1617da0fd
kachisapphire <jarapolice03@gmail.com>
2023-09-26 05:32:09 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,3 +1,20 @@
 #include "lists.h"
 /**
- *
+ *reverse_listint - function to execute
+ *@head: pointer to function
+ *Return: first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p = NULL, *c = *head, *temp;
+
+	while (c != NULL)
+	{
+		temp = c->next;
+		c->next = p;
+		p = c;
+		c = temp;
+	}
+	*head = p;
+	return (*head);
+}
