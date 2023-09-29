c21d46fd6cc1bfeb106f5949a03c316d19d42e13
kachisapphire <jarapolice03@gmail.com>
2023-09-28 21:42:46 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, j, sum = 0, p = 1, length;
-	int ba = 2;
+	unsigned int sum = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
-	for (length = 0, b[length] != '\0'; length++)
-		;
-	if (length == 1 && (b[0] == '0' || b[0] == '1')
-			return (b[0] - 48);
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-		return (0);
-		for (j = length - 1; j > 0; j--)
-		p = p * ba;
-		sum = sum + (p * (b[i] - 48));
-		length--;
-		p = 1;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		sum = 2 * sum + (b[i] - '0');
 	}
 	return (sum);
 }
