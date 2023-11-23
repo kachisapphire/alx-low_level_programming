9fb471a3ec67818cf6a2f7651f2c0e0af735eca3
kachisapphire <mychaelesther99gmail.com>
2023-11-23 12:13:35 -0800

    printbinary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_binary - function to execute
+ *@n: parameter
+ *Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, sum = 0;
+	unsigned long int count;
+
+	for (i = 63; i >= 0; i--)
+	{
+		count = n >> i;
+		if (count & 1)
+		{
+			_putchar('1');
+			sum++;
+		}
+		else if (sum)
+			_putchar('0');
+	}
+	if (!sum)
+		_putchar('0');
+}
