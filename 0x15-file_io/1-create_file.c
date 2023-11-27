23bdace4be277709f3fbe297295df0cafd57e5a4
kachisapphire <mychaelesther99gmail.com>
2023-11-27 08:43:52 -0800

    truncate

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *create_file - command to execute
+ *@filename: name of file to craete
+ *@text_content: null string
+ *Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int letters, i;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = " ";
+	for (letters = 0; text_content[letters]; letters++)
+		;
+	i = write(fd, text_content, letters);
+	if (i == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
