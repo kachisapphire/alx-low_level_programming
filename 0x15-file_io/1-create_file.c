83554cfc930c6789a4e01f27f7f216f4942ad5d6
kachisapphire <mychaelesther99gmail.com>
2023-11-27 09:01:51 -0800

    truncate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	for (letters = 0; text_content[letters]; letters++)
 		;
 	i = write(fd, text_content, letters);
