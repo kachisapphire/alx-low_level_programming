d59e063b911a9a5484182636754c2b7d0813c1ed
kachisapphire <mychaelesther99gmail.com>
2024-01-12 02:26:36 -0800

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *dlistint_len - function to execute
+ *@h: pointer 
+ *Return: nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
