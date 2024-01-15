f1720e2b31e834ce2c1697a4b8282fa82d2b4d78
kachisapphire <mychaelesther99gmail.com>
2024-01-15 07:14:52 -0800

    C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+ *add - function to execute
+ *@a: ..
+ *@b: ..
+ *Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - function to execute
+ *@a: ..
+ *@b: ..
+ *Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mod - function to execute
+ *@a: ..
+ *@b: ..
+ *Return: mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ *mul - function to execute
+ *@a: ..
+ *@b: ..
+Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *div - function to execute
+ *@a: ..
+ *@b: ..
+ *Return: div
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
