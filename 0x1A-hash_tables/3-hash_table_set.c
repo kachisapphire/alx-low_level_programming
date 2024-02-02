7d48e875a0f95a6b58ec560a85fb29da9ff8c572
kachisapphire <mychaelesther99@gmail.com>
2024-02-02 00:18:55 -0800

    editing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,24 +8,38 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx;
+	unsigned long int idx, i;
 	hash_node_t *node;
+	char *value_dup;
 
-	if (key == NULL || key[0] == '\0')
+	if (key == NULL || key[0] == '\0' || value == NULL)
+		return (0);
+	value_dup = strdup(value);
+	if (value_dup == NULL)
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if(strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_dup;
+			return (1);
+		}
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
-	if (node->key == NULL || node->value == NULL)
 	{
-		free(node->key);
-		free(node->value);
+		free(value_dup);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
 		free(node);
 		return (0);
 	}
+	node->value = value_dup;
 	node->next = ht->array[idx];
 	ht->array[idx] = node;
 	return (1);
